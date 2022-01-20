77e4895d37822322ec7ed5979a0ec5bd170f439f
kirryya <97147007+kirryya@users.noreply.github.com>
2022-01-20 16:42:30 +0300

    20/01

---
+++ src/components/Profile.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Profile = () => {
+    return <div className='content'>
+        <div>
+            <img
+                src='https://img.rawpixel.com/s3fs-private/rawpixel_images/website_content/upwk61813380-wikimedia-image.jpg?w=800&dpr=1&fit=default&crop=default&q=65&vib=3&con=3&usm=15&bg=F4F4F3&ixlib=js-2.2.1&s=35040e9874b05a262b0e5ffdd27806cf'/>
+        </div>
+        <div>
+            ava + description
+        </div>
+        <div>
+            My posts
+            <div>
+                New post
+            </div>
+            <div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
+        </div>
+    </div>
+}
+
+export default Profile;
