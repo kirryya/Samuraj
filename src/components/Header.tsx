8d98cecac66d8fc0fc3f942fa584bcc7c7db724d
kirryya <kir1987@inbox.ru>
2022-02-03 21:00:48 +0300

    delete

--- src/components/Header.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Header = () => {
-    return <header className='header'>
-        <img src='https://dinamo-minsk.by/assets/images/logo/logo.png'/>
-    </header>;
-}
-
-export default Header;
