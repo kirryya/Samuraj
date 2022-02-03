a009e239c8af9c7f21902e2c64512c94854e0dc4
kirryya <kir1987@inbox.ru>
2022-02-03 20:25:13 +0300

    modules

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import s from './MyPosts.module.css'
-import Post from "./Post/Post.tsx";
+import Post from "./Post/Post";
 
 
 const MyPosts = () => {
--- src/components/Profile/MyPosts/Post/Post.tsx
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-// @ts-ignore
 import s from './Post.module.css'
 
 
