e708ff808d07d7ee6e6b83c567ed8d2b5ea0007d
kirryya <kir1987@inbox.ru>
2022-02-03 22:46:53 +0300

    Routing

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,13 +2,12 @@ import React from "react";
 import s from "./Navbar.module.css";
 
 const Navbar = () => {
-
     return <nav className={s.nav}>
         <div className={s.item}>
-            <a>Profile</a>
+            <a href="/profile">Profile</a>
         </div>
         <div className={s.item}>
-            <a>Messages</a>
+            <a href="/dialogs">Messages</a>
         </div>
         <div className={s.item}>
             <a>News</a>
