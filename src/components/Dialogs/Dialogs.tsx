d4dc3244c2e22c5f4a9ee466f802b01f87b330eb
kirryya <kir1987@inbox.ru>
2022-02-06 10:54:05 +0300

    add Navlink

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
+import s from "./Dialogs.module.css";
 
 const Dialogs = () => {
     return (
-        <div>
-            <div>Messages 1</div>
-            <div>Messages 2</div>
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog}>
+                    Dimych
+                </div>
+                <div className={s.dialog}>
+                    Andrey
+                </div>
+                <div className={s.dialog}>
+                    Sveta
+                </div>
+                <div className={s.dialog}>
+                    Sasha
+                </div>
+                <div className={s.dialog}>
+                    Viktor
+                </div>
+                <div className={s.dialog}>
+                    Valera
+                </div>
+
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How are you?</div>
+                <div className={s.message}>Yo</div>
+            </div>
         </div>
     );
 };
