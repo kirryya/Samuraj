f51c04a1adefc022a3ee56e05e1dc5472eb9f1ac
kirryya <kir1987@inbox.ru>
2022-02-10 10:44:45 +0300

    props for Dialogs

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,17 +1,12 @@
 import React from "react";
 import MyPosts from "./MyPosts/MyPosts";
+import {ProfileInfo} from "./ProfileInfo/ProfileInfo";
+
 
 const Profile = () => {
     return (
-        <div>
-            <div>
-                <img
-                    src='https://img.rawpixel.com/s3fs-private/rawpixel_images/website_content/upwk61813380-wikimedia-image.jpg?w=800&dpr=1&fit=default&crop=default&q=65&vib=3&con=3&usm=15&bg=F4F4F3&ixlib=js-2.2.1&s=35040e9874b05a262b0e5ffdd27806cf'
-                    alt='Ромашка'/>
-            </div>
-            <div>
-                ava + description
-            </div>
+        <div >
+            <ProfileInfo/>
             <MyPosts/>
         </div>
     )
